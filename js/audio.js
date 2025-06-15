d4bfe6baca0287d6aa0467f42e4fca875acaa858
NerusenTakahashi <xynelsdesign@gmail.com>
2025-06-15 20:20:17 +0700

    first commit github

--- js/audio.js
+++ js/audio.js
@@ -1,11 +1,14 @@
-let myAudio = new Audio("https://cdn.pixabay.com/audio/2024/02/14/audio_b9bc3934cc.mp3"); // Create an Audio object with the file 'Ceylon.mp3'
+let myAudio = new Audio(
+  "https://open.spotify.com/track/2UgCs0i0rNHUH2jKE5NZHE?si=xRHCbazwTuCaecASlMhdPA"
+); // Create an Audio object with the file 'Ceylon.mp3'
 
-window.onload = function() {
-    setInterval(GameLoop, 1000 / 10); // Start the game loop when the window is loaded
+window.onload = function () {
+  setInterval(GameLoop, 1000 / 10); // Start the game loop when the window is loaded
 };
 
 function GameLoop() {
-    if (myAudio.paused) { // Check if the audio is paused
-        myAudio.play(); // If paused, play the audio
-    }
+  if (myAudio.paused) {
+    // Check if the audio is paused
+    myAudio.play(); // If paused, play the audio
+  }
 }
